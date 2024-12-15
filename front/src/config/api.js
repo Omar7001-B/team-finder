b14d67122c32c6e46bae95f71ab21ba381b7adf9
Abbas49 <abbasabdo735@gmail.com>
2024-12-15 23:43:11 +0200

    insert api url inside the code instead of .env

--- front/src/config/api.js
+++ front/src/config/api.js
@@ -1,5 +1,5 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+   "https://team-finder-back.vercel.app/" || "http://localhost:5000";
 
 export const endpoints = {
   auth: {
